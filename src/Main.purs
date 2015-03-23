8ec76eab3798b254a01c77eb25d8e348a1cd4827
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-03-23 08:57:16 +0100

    displaying stuff

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.Array (map)
+import Data.Array (map, null)
 import Data.String (joinWith, length)
 import Control.Monad.Eff
 import Control.Monad.Cont.Trans
@@ -10,37 +10,52 @@ import Data.JSON
 import qualified Data.Map as M
 import Data.Maybe
 import Control.Monad.Eff.DOM
+import Data.Array.Unsafe (tail)
 
-{-
 data Quad = Quad { subject :: String
                , predicate :: String
                , object :: String
                }
-instance u22FromJSON :: FromJSON u225 where
+               
+instance quadFromJSON :: FromJSON Quad where
     parseJSON (JObject o) = do
         b1 <- o .:  "subject"
         b2 <- o .: "predicate"
         b3 <- o .: "object"
         return $ Quad { subject: b1, predicate: b2, object: b3}
-    parseJSON _ = fail "u22 parse failed."
--}
+    parseJSON _ = fail "quad parse failed."
+
+instance quadToJSON :: ToJSON Quad where
+    toJSON (Quad { subject = b1, predicate = b2, object = b3 }) =
+        object ["subject" .= b1, "predicate" .= b2, "object" .= b3]
+
+lengthl :: forall a. [a] -> Number
+lengthl arr =
+    if null arr
+    then 0
+    else 1 + lengthl (tail arr)
 
 --Cannot unify Control.Monad.Eff.DOM.Node with Prelude.Unit.
---updateUI :: String -> forall eff. Eff (dom :: Control.Monad.Eff.DOM.DOM | eff) Unit
-updateUI json = do
+--updateUI :: Maybe (M.Map String String)  -> forall eff. Eff (dom :: Control.Monad.Eff.DOM.DOM | eff) Unit
+updateUI quad = do
     Just container <- querySelector ".container"
-    
 
-    setInnerHTML json container
+    let list = case quad of
+         Just quad -> M.values quad
+         Nothing -> []
+    ul <- createElement "ul"
+    foreachE list $ \element -> do
+      li <- createElement "li" >>= setText element
+      li `appendChild` ul
+      return unit
+
+    ul `appendChild` container
+ 
 
 main = runContT (getResponseText purescript_org) $ \response -> do
-  let text = decode response :: Maybe (M.Map String String)
-  let actualText =  case text of
-        Just sth -> M.showTree sth
-        Nothing -> "nothing"
-  
-  updateUI actualText
-  trace actualText
+  let quad = decode response :: Maybe (M.Map String String)
+  updateUI quad
+  trace "finished manipulating ui"
 
   where
   getResponseText req = responseToString <$> getAll req
